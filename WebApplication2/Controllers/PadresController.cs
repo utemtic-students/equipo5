6eaef5b84f845a9c86cf013f2b5f5f7f8218a6a1
Va20121225 <a20121225@utem.edu.mx>
2016-07-08 13:07:52 -0500

    Base de la rama

---
+++ WebApplication2/Controllers/PadresController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class PadresController : Controller
+    {
+        // GET: Padres
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Movimietos()
+        {
+            return View();
+        }
+
+        public ActionResult Recargas()
+        {
+            return View();
+        }
+
+        public ActionResult Contactanos()
+        {
+            return View();
+        }
+    }
+}
