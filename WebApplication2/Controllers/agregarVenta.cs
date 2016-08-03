3974a830255ef894580ad844898531caccd233ee
Eduardo Hernández <eduardo.truns@gmail.com>
2016-08-03 03:50:27 -0500

    Área de cafetería terminada
    Se ha completado todas las funciones del área de cafetería, solo queda
    ver el diseño de la sección.

---
+++ WebApplication2/Controllers/agregarVenta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Controllers
+{
+    public class agregarVenta
+    {
+        public void AgregarVenta(Venta venta)
+        {
+
+
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+
+
+                dc.Ventas.Add(venta);
+                dc.SaveChanges();
+
+
+            }
+        }
+    }
+}
