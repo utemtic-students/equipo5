b4b529aaaea6b85861e3963c264ac9bf128e5cc0
Eduardo Hernández <eduardo.truns@gmail.com>
2016-08-04 20:15:37 -0500

    se corrigio un error en login
    Las imagenes ahora se muestran en la pagina de login

--- WebApplication2/Controllers/CafeteriaController.cs
+++ WebApplication2/Controllers/CafeteriaController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
 
 namespace WebApplication2.Controllers
 {
@@ -26,17 +27,35 @@ namespace WebApplication2.Controllers
 
                 var saldo = dc.Padres.Where(a => a.Id.Equals(idPadre)).FirstOrDefault();
                 ViewData["Saldo"] = saldo.Saldo;
+                string name = saldo.Nombre;
                 if (saldo.Saldo > cantidad) {
 
                     var calculo = (saldo.Saldo - cantidad);
                     saldo.Saldo = calculo;
                     dc.SaveChanges();
                 }
-                else
-                {
-                    
+                if (saldo.Saldo > 50)
+                {/*
+                    PENDIENTE!!!cambiar la tabla padres y anexar los datos a la tabla users, asi mismo
+                    agregar los campos faltantes a la tabla users.
+
+                    MailMessage mail = new MailMessage();
+
+                    mail.To.Add(ejemplo@gmail.com); //destinatario
+                    mail.From = new MailAddress("kidscoinservices@gmail.com");
+                    mail.Subject = "Saldo de su cuenta"; // escribir asunto
+                    mail.Body = "Hola, " + name+".\n"+" Este mensaje se genera automáticamente par avisarle que su saldo en KidsCoin esta por terminarse, agrege más credito a su cuenta para que sus hijos puedan seguir utilizando su credito."; // escribir texto.
+
+
+                    mail.IsBodyHtml = true;
+
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Credentials = new System.Net.NetworkCredential("kidscoinservices@gmail.com", "utmanzanillo");
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                    */
                 }
-                
                 
             }
         }
@@ -80,6 +99,15 @@ namespace WebApplication2.Controllers
         [AuthorizeUserAccessLevel(UserRole = "cafeteria")]
         public ActionResult Cafeteria()
         {
+            MyDatabaseEntities dc = new MyDatabaseEntities();
+
+            string CurrentUser = HttpContext.User.Identity.Name.ToString();
+            if (CurrentUser != "")
+            {
+                var res = dc.Users.Where(a => a.Username.Equals(CurrentUser)).FirstOrDefault();
+                ViewData["rol"] = res.Area;
+            }
+
             return View();
         }
         [AuthorizeUserAccessLevel (UserRole = "cafeteria")]
