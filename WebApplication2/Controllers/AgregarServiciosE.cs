8d8c1281380355d77466dc5e89f8088e036c45f2
Jhoana Aldana <a20130559@utem.edu.mx>
2016-08-14 13:37:10 -0500

    Formulariosderesgistro|cafeteria|Servicios

---
+++ WebApplication2/Controllers/AgregarServiciosE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Controllers
+{
+    public class AgregarServiciosE
+    {
+        public void AgregarServicioE(User user)
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                dc.Users.Add(user);
+                dc.SaveChanges();
+            }
+        }
+    }
+}
