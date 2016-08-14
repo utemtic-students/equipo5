119cc7c743744a8fded0dee44e40093b1f84406a
Eduardo Hernández <eduardo.truns@gmail.com>
2016-08-13 21:37:49 -0500

    Se corrigo la tabla de hijos en pagina de padres

--- WebApplication2/Controllers/PadresController.cs
+++ WebApplication2/Controllers/PadresController.cs
@@ -54,13 +54,10 @@ namespace WebApplication2.Controllers
         public PartialViewResult Hijos(int id)
         {
 
-            var rv = new List<Alumno>();
-            using (MyDatabaseEntities dc = new MyDatabaseEntities())
-            {
-                var res = dc.Alumnos.Where(a => a.Id_Padre == id ).FirstOrDefault();
-               // List<Alumno> ls = new List<Alumno>();
-                rv.Add(res);
-            }
+            MyDatabaseEntities dc = new MyDatabaseEntities();
+            var res = dc.Alumnos.Where(a => a.Id_Padre == id).ToList();
+            var rv = res;
+
             return PartialView(rv);
         }
 
